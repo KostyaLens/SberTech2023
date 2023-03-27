3125ebadc5acc21aab733190ca258550d9cc4313
KostyaLens <124429244+KostyaLens@users.noreply.github.com>
2023-03-27 20:33:05 +0300

    Add files via upload

---
+++ Java Collections 2/Homework/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Statistic statistic = new Statistic("KLUEV.txt");
+        statistic.printStatistic();
+
+    }
+}
