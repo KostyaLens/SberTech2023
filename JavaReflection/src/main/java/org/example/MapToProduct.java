5cc7e767ed6c169cc5a16652a059f58e12ebdf53
KostyaLens <124429244+KostyaLens@users.noreply.github.com>
2023-04-23 10:13:12 +0300

    Commit

--- JavaReflection/src/main/java/org/example/MapToProduct.java
+++ JavaReflection/src/main/java/org/example/MapToProduct.java
@@ -8,30 +8,25 @@ public class MapToProduct {
         Class clazz = pet.getClass();
         Animal animal = new Animal();
         try {
-            Field TitleField = clazz.getDeclaredField("name");
-            TitleField.setAccessible(true);
-            String title = (String) TitleField.get(pet);//взяли кличку из класса pet
-            Field StatusField = clazz.getDeclaredField("status");
-            StatusField.setAccessible(true);
-            Status status = (Status) StatusField.get(pet);//взяли Статус из класса pet
+            Field titleField = clazz.getDeclaredField("name");
+            titleField.setAccessible(true);
+            String title = (String) titleField.get(pet);//взяли кличку из класса pet
+            Field statusField = clazz.getDeclaredField("status");
+            statusField.setAccessible(true);
+            Status status = (Status) statusField.get(pet);//взяли Статус из класса pet
             Field photoListField = clazz.getDeclaredField("photosList");
             photoListField.setAccessible(true);
             List<Photo> photos = (List<Photo>) photoListField.get(pet); //взяли Фото из класса pet
             clazz = animal.getClass();
-            TitleField = clazz.getDeclaredField("title");
-            TitleField.setAccessible(true);
-            TitleField.set(animal, title);// установили кличку пета энималу
-            StatusField = clazz.getDeclaredField("isAvailable");
-            Field StatusField2 = clazz.getDeclaredField("isSold");
-            StatusField2.setAccessible(true);
-            StatusField.setAccessible(true);
-            if (status == Status.AVAILABLE){// установили статус пета энималу
-                StatusField.set(animal, true);
-                StatusField2.set(animal, false);
-            }else{
-                StatusField.set(animal, false);
-                StatusField2.set(animal, true);
-            }
+            titleField = clazz.getDeclaredField("title");
+            titleField.setAccessible(true);
+            titleField.set(animal, title);// установили кличку пета энималу
+            statusField = clazz.getDeclaredField("isAvailable");
+            Field statusField2 = clazz.getDeclaredField("isSold");
+            statusField2.setAccessible(true);
+            statusField.setAccessible(true);
+            statusField.set(animal, status == Status.AVAILABLE);
+            statusField2.set(animal, status == Status.SOLD);
             HashMap<String, String> photosMap = new HashMap<>();
             photoListField = clazz.getDeclaredField("photosMap");
             photoListField.setAccessible(true);
--- JavaReflection/src/test/java/org/example/MapToProductTest.java
+++ JavaReflection/src/test/java/org/example/MapToProductTest.java
@@ -9,54 +9,24 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class MapToProductTest {
+    @org.junit.Test
+    public void testMapToProductAvailable(){
+        Pet cat = new Pet("Барсик", Status.AVAILABLE, List.of(new Photo("Барсик с цветочком","https://placekitten.com/200/300"),
+                new Photo("Барсик на ручках", "https://placekitten.com/500/605")));
+        MapToProduct mapToProduct = new MapToProduct();
+        Animal animal = mapToProduct.mapToProductDto(cat);
+        Assert.assertEquals("Барсик Available {Барсик с цветочком=https://placekitten.com/200/300, Барсик на ручках=https://placekitten.com/500/605}",
+                animal.toString());
+    }
 
     @org.junit.Test
-    public void mapToProductDto() throws NoSuchFieldException, IllegalAccessException {
-        int i = 0;
+    public void testMapToProductSold(){
+        Pet cat = new Pet("Барсик", Status.SOLD, List.of(new Photo("Барсик с цветочком","https://placekitten.com/200/300"),
+                new Photo("Барсик на ручках", "https://placekitten.com/500/605")));
         MapToProduct mapToProduct = new MapToProduct();
-        Pet cat = new Pet("Барсик", Status.AVAILABLE,
-                List.of(new Photo("Барсик с цветочком","https://placekitten.com/200/300"),
-                        new Photo("Барсик на ручках", "https://placekitten.com/500/605")));
         Animal animal = mapToProduct.mapToProductDto(cat);
-        Class clasz = cat.getClass();
-        Field TitleField = clasz.getDeclaredField("name");
-        TitleField.setAccessible(true);
-        String name = (String)TitleField.get(cat);
-        Class clazz = animal.getClass();
-        TitleField = clazz.getDeclaredField("title");
-        TitleField.setAccessible(true);
-        String title = (String)TitleField.get(animal);
-        Assert.assertEquals(name, title);
-        Field StatusField = clasz.getDeclaredField("status");
-        StatusField.setAccessible(true);
-        Status s = (Status) StatusField.get(cat);
-        StatusField = clazz.getDeclaredField("isAvailable");
-        StatusField.setAccessible(true);
-        boolean status1 = (boolean) StatusField.get(animal);
-        StatusField = clazz.getDeclaredField("isSold");
-        StatusField.setAccessible(true);
-        boolean status2 = (boolean) StatusField.get(animal);
-        if (s == Status.AVAILABLE) {
-            Assert.assertEquals(true, status1);
-            Assert.assertEquals(false, status2);
-        }else {
-            Assert.assertEquals(false, status1);
-            Assert.assertEquals(true, status2);
-        }
-        clasz = cat.getClass();
-        Field photoListField = clasz.getDeclaredField("photosList");
-        photoListField.setAccessible(true);
-        List<Photo> photos = (List<Photo>) photoListField.get(cat);
-        Field photoMapField = clazz.getDeclaredField("photosMap");
-        photoMapField = clazz.getDeclaredField("photosMap");
-        photoMapField.setAccessible(true);
-        HashMap<String, String> photosMap = (HashMap<String, String>) photoMapField.get(animal);
-        for (String key: photosMap.keySet()){
-            Photo photo = photos.get(i);
-            Assert.assertEquals(key, photo.getName());
-            Assert.assertEquals(photosMap.get(key), photo.getURL());
-            i++;
-        }
+        Assert.assertEquals("Барсик Sold {Барсик с цветочком=https://placekitten.com/200/300, Барсик на ручках=https://placekitten.com/500/605}",
+                animal.toString());
     }
 
 }
