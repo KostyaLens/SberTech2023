4c6f9642706a07274661afd01051f386713c23bd
KostyaLens <124429244+KostyaLens@users.noreply.github.com>
2023-05-16 15:54:34 +0300

    Commit

--- Annotation/src/main/java/org/example/Main.java
+++ Annotation/src/main/java/org/example/Main.java
@@ -8,10 +8,12 @@ public class Main {
     }
 
     public static void Print(Massages clazz){
+        Class claz = clazz.getClass();
         Method[] methods = clazz.getClass().getDeclaredMethods();
         for (Method method: methods){
             method.setAccessible(true);
             if (method.isAnnotationPresent(Paint.class)){
+                String style = method.getAnnotation(Paint.class).style();
                 try {
                     System.out.println(method.getAnnotation(Paint.class).color() + method.getAnnotation(Paint.class).style() +
                             method.invoke(clazz) + Color.RESET);
@@ -21,7 +23,7 @@ public class Main {
                 }
             }else if(clazz.getClass().isAnnotationPresent(Paint.class)){
                 try {
-                    System.out.println(clazz.getClass().getAnnotation(Paint.class).color() + clazz.getClass().getAnnotation(Paint.class).style() +
+                    System.out.println(clazz.getClass().getAnnotation(Paint.class).style() + clazz.getClass().getAnnotation(Paint.class).style() +
                             method.invoke(clazz) + Color.RESET);
                 }catch (Exception e){
                     System.out.println("Поизошла ошибка" + e);
