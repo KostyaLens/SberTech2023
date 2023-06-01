b9b26950c93b45e8f52a5fe7eeda0647ca1f8223
KostyaLens <124429244+KostyaLens@users.noreply.github.com>
2023-06-01 19:18:23 +0300

    Commit and push

--- Philosophy/src/main/java/org/example/Main.java
+++ Philosophy/src/main/java/org/example/Main.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Philosopher[] philosophers = new Philosopher[5];
+        Fоrk[] fоrks = new Fоrk[5];
+        for (Fоrk fоrk: fоrks ){
+            fоrk.taken = false;
+        }
+        for (int i = 0; i < philosophers.length; i++) {
+            Philosopher philosopher = new Philosopher(fоrks[i], fоrks[(i + 1) % 5]);
+        }
+        Thread thread0 = new Thread(philosophers[0], "Плутон");
+        Thread thread1 = new Thread(philosophers[1], "Аристотель");
+        Thread thread2 = new Thread(philosophers[2], "Оботуров");
+        Thread thread3 = new Thread(philosophers[3], "укуренный Олежа");
+        Thread thread4 = new Thread(philosophers[4], "пьяный Игорь");
+        thread0.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
     }
+
+
 }
--- Philosophy/src/main/java/org/example/Philosopher.java
+++ Philosophy/src/main/java/org/example/Philosopher.java
@@ -1,14 +1,24 @@
 package org.example;
 
-import java.util.concurrent.locks.Lock;
-
-public class Philosopher {
-    private Object leftFork;
-    private Object rightFork;
-    public Philosopher(Object leftFork, Object rightFork){
+class Fоrk {
+    public boolean taken;
+}
+public class Philosopher implements Runnable {
+    private Fоrk leftFork;
+    private Fоrk rightFork;
+    public Philosopher(Fоrk leftFork, Fоrk rightFork){
         this.leftFork = leftFork;
         this.rightFork = rightFork;
     }
-}
 
-public 
+    @Override
+    public void run() {
+        while (true){
+//            if (Thread.// я хотел получить вот так доспуп взята ли вилка если да то философ думает, и ждеёт пока осободиться)
+//                if //здесь я хотел проверить зята ли второя если то он кладёт ту что взял и думает
+                    //а здесь типо есть а потом кладёт обез вилки
+                // взята ли вилка я хотел контролировать полем taken у Fork
+            //
+        }
+    }
+}
